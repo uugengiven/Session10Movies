5999c92de40d965555bb617dce65f9c9df8f56f4
John Lange <lange.john.m@gmail.com>
2020-05-06 16:40:40 -0400

    Start your movies

--- src/App.js
+++ src/App.js
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
 
+    this.state = {
+      movies: []
+    }
+
+    this.loadMovies = this.loadMovies.bind(this);
+  }
+
+  componentDidMount()
+  {
+    this.loadMovies();
+  }
+
+  loadMovies()
+  {
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=b6fbc7f3f313bd395902af464ef47262&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+    .then(response => {
+      return response.json();
+    })
+    .then(jsonData => {
+        console.log(jsonData);
+        this.setState({movies: jsonData.results})
+    });
+  }
+
+  makeMovieTitle(movie)
+  {
+    return <h1>{movie.title} ({movie.vote_average})</h1>
+  }
+
+  render() {
+    let displayMovies = [];
+    this.state.movies.forEach((item) => {
+      displayMovies.push(this.makeMovieTitle(item));
+    });
+
+    displayMovies = this.state.movies.map(this.makeMovieTitle);
+
+    // use either a forEach or a map, both aren't needed
+
+    return (
+      <div className="App">
+        <h1>I have loaded {this.state.movies.length} movies!</h1>
+        {displayMovies}
+      </div>
+    );
+  }
+}
 export default App;
